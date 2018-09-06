016b6fb89c7708da009c3797e8d1d02743e6396e
Britney Smith <brit.smith7@gmail.com>
2018-09-06 16:27:30 -0400

    Initial commit. Added data models and added ContactListTableViewController

---
+++ Contacts/Contacts/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Britney Smith on 9/6/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+    var name: String
+    var phoneNumber: String
+}
---
+++ Contacts/Contacts/ContactList.swift
@@ -0,0 +1,14 @@
+//
+//  ContactList.swift
+//  Contacts
+//
+//  Created by Britney Smith on 9/6/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+class ContactList {
+    var contacts = ["Jane Doe":"313-555-1212",
+                    "Lisa Vance":"313-888-3131"]
+}
